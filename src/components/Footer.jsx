d537ebfae49522e40937512bf581d048b9ded7a4
Abhi8756 <abhijit.potty@gmail.com>
2025-07-19 23:05:12 +0530

    minor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="text-center w-full h-16 bg-gray-800 text-white">Made with ❤️ by Abhijit</div>
+    <div className="flex items-center justify-center w-full h-16 bg-gray-800 text-white">Made with ❤️ by Abhijit</div>
   )
 }
 
